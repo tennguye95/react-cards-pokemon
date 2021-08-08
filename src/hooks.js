7520d5c186c5a8c20975b4f7b8f0e382266d5d5f
sacred95 <sacred9529@gmail.com>
2021-08-08 08:41:46 -0700

    finalized

---
+++ src/hooks.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useFlip(initialFlipState = true) {
+  const [isFlipped, setFlipped] = useState(initialFlipState);
+
+  const flip = () => {
+    setFlipped(isUp => !isUp);
+  };
+
+  return [isFlipped, flip];
+}
+
+function useAxios(keyInLS, baseUrl) {
+  const [responses, setResponses] = useLocalStorage(keyInLS);
+
+  const addResponseData = async (formatter = data => data, restOfUrl = "") => {
+    const response = await axios.get(`${baseUrl}${restOfUrl}`);
+    setResponses(data => [...data, formatter(response.data)]);
+  };
+
+  const clearResponses = () => setResponses([]);
+
+  return [responses, addResponseData, clearResponses];
+}
+
+function useLocalStorage(key, initialValue = []) {
+  if (localStorage.getItem(key)) {
+    initialValue = JSON.parse(localStorage.getItem(key));
+  }
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+}
+
+export default useLocalStorage;
+
+export { useFlip, useAxios, useLocalStorage };
